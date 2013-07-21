8b8033c1a6e00db239e005a3dab046b935030365
Cole Pilegard <c.pilegard@gmail.com>
2013-07-20 17:58:20 -0700

    initial commit

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,9 @@
 get '/' do
   # render home page
  #TODO: Show all users if user is signed in
+  if session[:user]
+    @users = User.all?
+  end
   erb :index
 end
 
@@ -28,4 +31,14 @@ end
 
 post '/users' do
   # sign-up a new user
+  name = params['user'['name']]
+  email = params['user'['email']]
+  password = params['user'['password']]
+  password_salt = BCrypt::Engine.generate_salt
+  password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+
+  user = User.create({ name: name, email: email, password_hash: password_hash })
+  if user.errors
+    session[:errors] = user.errors.full_messages
+  end
 end
